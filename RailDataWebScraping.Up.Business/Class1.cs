ac2988792f18f2d5a53ace67b70ad7fd984b3c8d
Adrian Ordonez Tostega <adriantostega@hotmail.com>
2018-09-08 18:25:28 -0700

    ATOS - Added console project and business library for Union Pacific.

---
+++ RailDataWebScraping.Up.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RailDataWebScraping.Up.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ RailDataWebScraping.Up.Service/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RailDataWebScraping.Up.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+        }
+    }
+}
